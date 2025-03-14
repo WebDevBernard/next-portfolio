9f8d4717f7ac018243f5440aaea97970d3728a18
Bernard Yang <ybernard@gmail.com>
2025-03-13 18:43:34 -0700

    remove md

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,18 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    extends: ["next"],
     rules: {
-      "@typescript-eslint/ban-ts-comment": "warn",
-      "@typescript-eslint/no-empty-object-type": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          vars: "all",
-          args: "after-used",
-          ignoreRestSiblings: false,
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          destructuredArrayIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^(_|ignore)",
-        },
-      ],
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
     },
-  },
-  {
-    ignores: [".next/"],
-  },
+  }),
 ];
 
 export default eslintConfig;
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,8 +4,6 @@ import { MetadataRoute } from "next";
 export const BASE_URL = "https://bernardyang.com";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const posts = await getPostsSitemap();
-
   return [
     {
       url: BASE_URL,
@@ -13,12 +11,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: BASE_URL + "/contact-me",
     },
-    {
-      url: BASE_URL + "/content",
-    },
-    ...posts.map((post) => ({
-      url: `${BASE_URL}/content/${post.slug}`,
-      lastModified: post.mtime,
-    })),
   ];
 }
