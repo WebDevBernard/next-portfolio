6f9a9adc672b8b5dd37c30cc97ef1c1f728183af
Bernard Yang <ybernard@gmail.com>
2025-03-16 00:52:28 -0700

    remove context menu

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,14 +2,7 @@
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
-import { avatarSublink, githubAvatar } from "@/lib/data";
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuTrigger,
-} from "@/components/ContextMenu";
-import Link from "next/link";
+import { githubAvatar } from "@/lib/data";
 const Avatar = () => {
   const [bounce, setBounce] = useState(false);
 
@@ -19,28 +12,19 @@ const Avatar = () => {
   };
 
   return (
-    <ContextMenu>
-      <ContextMenuTrigger>
-        <Image
-          width={90}
-          height={90}
-          className={cn(
-            "rounded-lg border-2 border-zinc-700 relative cursor-pointer",
-            bounce && "animate-[wiggle_1s_ease-in-out]"
-          )}
-          src={githubAvatar}
-          alt="github_avatar"
-          onClick={handleClick}
-          loading="eager"
-          unoptimized
-        />
-      </ContextMenuTrigger>
-      <ContextMenuContent>
-        <ContextMenuItem>
-          <Link href={avatarSublink}>Click to play game</Link>
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+    <Image
+      width={90}
+      height={90}
+      className={cn(
+        "rounded-lg border-2 border-zinc-700 relative cursor-pointer",
+        bounce && "animate-[wiggle_1s_ease-in-out]"
+      )}
+      src={githubAvatar}
+      alt="github_avatar"
+      onClick={handleClick}
+      loading="eager"
+      unoptimized
+    />
   );
 };
 
