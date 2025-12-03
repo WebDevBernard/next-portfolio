2d262f707b3748343bdcb9932b1cd58435916fa0
unknown <ybernard@gmail.com>
2025-12-02 22:15:40 -0800

    add caption

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Page() {
       <div>
         <Avatar />
         <p className="flex justify-end px-2 pt-1 text-xs">
-          Picture of an AI Generated Bulldog
+          Picture of an AI Generated Turkey
         </p>
       </div>
 
