9aaa772ac945c8d65ce1c709271e3209428978b4
unknown <ybernard@gmail.com>
2025-12-03 22:11:28 -0800

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mb-8 flex justify-center pr-8 lg:mx-auto">
+    <footer className="mb-8 flex justify-center lg:mx-auto">
       <p className="mt-8 text-zinc-700">© {new Date().getFullYear()}</p>
     </footer>
   );
